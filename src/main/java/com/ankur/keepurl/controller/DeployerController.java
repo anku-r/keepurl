b109d88ad50e6291ac1a24a916565f18ce6158e9
Ankur Sharma <core2411>
2022-04-01 21:49:18 +0530

    Testing auth

--- src/main/java/com/ankur/keepurl/controller/DeployerController.java
+++ src/main/java/com/ankur/keepurl/controller/DeployerController.java
@@ -2,6 +2,7 @@ package com.ankur.keepurl.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public class DeployerController {
 	private static Logger logger = LoggerFactory.getLogger(DeployerController.class);
 
 	@PostMapping
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public void deploy(@RequestBody String payload) {
 		logger.info("Deployment OK: " + payload);
 	}
