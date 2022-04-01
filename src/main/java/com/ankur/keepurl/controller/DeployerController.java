c1a1d2755f120751ff3dd021ea065e9f50d33c73
Ankur Sharma <core2411>
2022-04-02 00:42:28 +0530

    Testing payload

--- src/main/java/com/ankur/keepurl/controller/DeployerController.java
+++ src/main/java/com/ankur/keepurl/controller/DeployerController.java
@@ -1,6 +1,7 @@
 package com.ankur.keepurl.controller;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +24,12 @@ public class DeployerController {
 
 	@PostMapping
 	@PreAuthorize("hasAuthority('ADMIN')")
-	public String deploy(@RequestBody String payload) throws IOException {
-		logger.info("Calling Deployment Script");
-		Runtime.getRuntime().exec(env.getProperty("devop.dir")+"/deploy.sh");
-		return "Deployment Triggered";
+	public String deploy(@RequestBody LinkedHashMap<String, Object> payload) throws IOException {
+		if (payload.get("ref").equals("refs/head/main")) {
+			logger.info("Calling Deployment Script");
+			Runtime.getRuntime().exec(env.getProperty("devop.dir")+"/deploy.sh");
+			return "Deployment Triggered";
+		}
+		return "Deployment Skipped";
 	}
 }
