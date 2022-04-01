7232e12a215714daac7f5e9ac6e9b8832e7f1987
Ankur Sharma <core2411>
2022-04-01 23:24:27 +0530

    Corrected DB Export

--- devop/deploy.sh
+++ devop/deploy.sh
@@ -7,8 +7,8 @@ git pull origin main
 echo "Executing maven build"
 mvn clean install -Dmaven.test.skip=true
 
-echo "Setting Environment Properties"
-/env_prop/exportProperties.sh
+echo "Exporting Datasource"
+export MONGO_KEEPURL_DATASOURCE=`cat /env_prop/MONGO_KEEPURL_DATASOURCE`
 
 echo "Stopping application"
 kill `ps -ef | awk '$10 ~ /keepurl/{print $2}'`
