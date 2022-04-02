db917a26ed8e7a627da0f0ac57248b69f638d80b
Ankur Sharma <core2411>
2022-04-02 18:05:04 +0530

    Logging

--- devop/deploy.sh
+++ devop/deploy.sh
@@ -1,5 +1,6 @@
 cd ..
 git pull origin main
 mvn clean install -Dmaven.test.skip=true
-export MONGO_KEEPURL_DATASOURCE=`cat /appinfo/MONGO_KEEPURL_DATASOURCE`
+export MONGO_KEEPURL_DATASOURCE=`cat /appinfo/prop/MONGO_KEEPURL_DATASOURCE`
+export LOG_LEVEL=`cat /appinfo/prop/LOG_LEVEL`
 java -jar target/keepurl-0.0.1-SNAPSHOT.jar
