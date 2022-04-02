f41e3ef9910ef1bec1ebd4ce22fe8072d7248b3e
Ankur Sharma <core2411>
2022-04-02 17:52:38 +0530

    Deployment refactoring

--- devop/deploy.sh
+++ devop/deploy.sh
@@ -1,13 +1,5 @@
-echo "Deploying Keep URL Application"
-echo "Pulling latest source from repository using branch: main"
 cd ..
 git pull origin main
-echo "Executing maven build"
 mvn clean install -Dmaven.test.skip=true
-echo "Exporting Datasource"
-export MONGO_KEEPURL_DATASOURCE=`cat /env_prop/MONGO_KEEPURL_DATASOURCE`
-echo "Stopping application"
-kill `ps -ef | awk '$10 ~ /keepurl/{print $2}'`
-echo "Starting application"
+export MONGO_KEEPURL_DATASOURCE=`cat /appinfo/MONGO_KEEPURL_DATASOURCE`
 java -jar target/keepurl-0.0.1-SNAPSHOT.jar
-echo "Deployment Complete"
