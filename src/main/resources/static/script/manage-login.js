4a5ba2005eae6e59ce6072b3cb631155a23e256c
Ankur Sharma <core2411>
2022-05-29 13:22:49 +0530

    changed login script

--- src/main/resources/static/script/manage-login.js
+++ src/main/resources/static/script/manage-login.js
@@ -1,19 +1,18 @@
 const RED = "red";
 const GREEN = "green"
 
-$(document).ready(function () {
-	
-	var urlParams = new URLSearchParams(window.location.search);
-	if (urlParams.has('error')) {
-		setMessage("Invalid username or password", RED);
-	} else if (urlParams.has('logout')) {
-		setMessage("You have been logged out", GREEN);
-	} else if (urlParams.has('no-user')) {
-	    setMessage("User does not exist", RED);
-	}
+const setMessage = function(message, color) {
+	$("#msg").css("color", color);
+	$("#msg").html(message);
+}
 
-	const setMessage = function(message, color) {
-		$("#msg").css("color", color);
-		$("#msg").html(message);
+$(document).ready(function () {	
+	param = window.location.search;
+	if (param.endsWith('error')) {
+		setMessage("Invalid username or password", RED);
+	} else if (param.endsWith('logout')) {
+		setMessage("You have been logged out", GREEN);
+	} else if (param.endsWith('no-user')) {
+	    setMessage("User does not exist", RED);
 	}
 });
