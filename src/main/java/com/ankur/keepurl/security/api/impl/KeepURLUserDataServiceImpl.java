0fa5d199e2dcfce38addc90e438b04c3f18c19c1
Ankur Sharma <core2411>
2023-01-06 18:24:45 +0530

    Added username in login exception

--- src/main/java/com/ankur/keepurl/security/api/impl/KeepURLUserDataServiceImpl.java
+++ src/main/java/com/ankur/keepurl/security/api/impl/KeepURLUserDataServiceImpl.java
@@ -29,7 +29,7 @@ public class KeepURLUserDataServiceImpl implements KeepURLUserDataService {
     public UserDetails loadUserByUsername(String username) {
         Optional<UserData> userData = repository.findById(username);
         if (!userData.isPresent()) {
-            throw new UsernameNotFoundException("User Not Found");
+            throw new UsernameNotFoundException("User '" + username + "' not found");
         }
         return mapper.mapEntityToUserDetail(userData.get());
     }
